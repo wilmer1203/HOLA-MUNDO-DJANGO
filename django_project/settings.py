ea61a0666f6216fdbaf87433e73bc329075c8463
Wilmer <wilmeraguirre2002.3@gmail.com>
2025-03-01 11:59:19 -0400

    primer commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # le decimos a django que busque archivos estaticos en la carpeta static
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
